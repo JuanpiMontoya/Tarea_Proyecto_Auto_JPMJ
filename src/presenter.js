57c943f500e31f9edad29ac10bfb59005b2744a1
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-09-03 18:29:37 -0400

    Proyecto corregido sin mostrar al auto

--- src/presenter.js
+++ src/presenter.js
@@ -3,7 +3,6 @@ import auto from "./Auto"
 const comandos = document.querySelector("#comandos");
 const form = document.querySelector("#auto-form");
 const posInicial = document.querySelector("#pos-inicial");
-const superficie = document.querySelector("#superficie-auto");
 const auto_Proyecto = new auto();
 
 form.addEventListener("submit", (event) => {
@@ -13,20 +12,6 @@ form.addEventListener("submit", (event) => {
     ComandoAuto = comandos.value.split("/");
     //Dividimos el comando y mostramos la posicion inicial
     posInicial.textContent = auto_Proyecto.getPosIni();
-    for (var i = 0; i < auto_Proyecto.getDimY(); i++) 
-    {
-      for (var j = 0; j < auto_Proyecto.getDimX(); j++) 
-      {
-        const celda = document.createElement('div');
-        celda.className = 'celda';
-        celda.id= 'celda_ ' + i + '_' + j;
-        celda.textContent = 'Elemento ' + i + '_' + j;
-        celda.style.display = 'inline-block';
-        superficie.appendChild(celda);        
-      }
-      const salto = document.createElement('br');
-      superficie.appendChild(salto);
-    }
   }
   else
   {
